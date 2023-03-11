df4c7cd930c0f8b2c68183e58b286c9af98339b7
영빈 <>
2023-03-11 13:42:07 +0900

    feat: delete other logics

--- note.js
+++ note.js
@@ -2,48 +2,101 @@ const audioContext = new AudioContext();
 const source = audioContext.createBufferSource();
 const gainNode = audioContext.createGain();
 
+// 노래 시작시간 3초 뒤
+// samurai 첫 음 시작하는 시간 3.03 ~ 3.04 큰 음은 3.06
+// 두번째음 3.75
+// note가 canvas 끝에 닿는 시간 처음 입력되는 시간 은 2.5? 2.60 ~ 2.61
 const notes = [
   {
-    time: 4,
-    key: "a",
-    color: "red",
-    startPosition: 0,
-    positionY: 0,
-  },
-  {
-    time: 4,
+    time: 0.6,
     key: "s",
-    color: "blue",
-    startPosition: 0,
     positionY: 0,
   },
   {
-    time: 5,
+    time: 0.65,
     key: "d",
-    color: "yellow",
-    startPosition: 0,
     positionY: 0,
   },
   {
-    time: 6,
+    time: 1.2,
+    key: "f",
+    positionY: 0,
+  },
+  {
+    time: 1.25,
     key: "j",
-    color: "black",
-    startPosition: 0,
     positionY: 0,
   },
   {
-    time: 4,
+    time: 1.95,
     key: "k",
-    color: "purple",
-    startPosition: 0,
     positionY: 0,
   },
   {
-    time: 5,
+    time: 2,
     key: "l",
-    color: "white",
-    startPosition: 0,
     positionY: 0,
+  },
+  {
+    time: 2.3,
+    key: "k",
+    positionY: 0,
+  },
+  {
+    time: 2.35,
+    key: "l",
+    positionY: 0,
+  },
+  {
+    time: 2.95,
+    key: "s",
+    positionY: 0,
+  },
+  {
+    time: 3,
+    key: "d",
+    positionY: 0,
+  },
+  {
+    time: 3.25,
+    key: "f",
+    positionY: 0,
+  },
+  {
+    time: 3.35,
+    key: "j",
+    positionY: 0,
+  },
+  {
+    time: 3.95,
+    key: "s",
+    positionY: 0,
+  },
+  {
+    time: 4.05,
+    key: "d",
+    positionY: 0,
+  },
+  {
+    time: 4.55,
+    key: "f",
+    positionY: 0,
+  },
+  {
+    time: 4.65,
+    key: "j",
+    positionY: 0,
+  },
+  {
+    time: 4.95,
+    key: "f",
+    positionY: 0,
+  },
+  {
+    time: 5.95,
+    key: "j",
+    positionY: 0,
+    height: 40,
   },
 ];
 
@@ -54,24 +107,125 @@ gainSlider.addEventListener("input", () => {
 
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
-const keys = ["a", "s", "d", "j", "k", "l"];
-const COLUMN_RGB_COLORS = ["255, 87, 34", "0, 188, 212", "246, 215, 67"];
+const keys = ["s", "d", "f", "j", "k", "l"];
 const MILISECOND = 1000;
 
 const height = canvas.height;
 const columnWidth = canvas.width / keys.length;
 const noteHeight = 20;
-
 const speed = 300;
 let time = 0;
-let positionY = 0;
-let positionX;
 
 const startButton = document.getElementById("start");
 const stopButton = document.getElementById("stop");
 const fileInput = document.querySelector("input[type='file']");
 
 let decodedBuffer;
+let delta;
+
+function updateNotes() {
+  const now = Date.now();
+
+  if (!delta) {
+    delta = now;
+  }
+
+  ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+  time += (now - delta) / MILISECOND;
+
+  const visibleNotes = notes.filter((note) => note.time <= time);
+  renderNotes(now, delta, visibleNotes);
+  requestAnimationFrame(updateNotes);
+
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "s") {
+      console.log(time, "s");
+    }
+
+    if (e.key === "d") {
+      console.log(time, "d");
+    }
+
+    if (e.key === "f") {
+      console.log(time, "f");
+    }
+
+    if (e.key === "j") {
+      console.log(time, "j");
+    }
+
+    if (e.key === "k") {
+      console.log(time, "k");
+    }
+    if (e.key === "l") {
+      console.log(time, "l");
+    }
+  });
+
+  delta = now;
+}
+
+function renderNotes(now, delta, notes) {
+  const notesArray = notes.sort((prev, next) => prev.time - next.time);
+
+  notesArray.forEach((note) => {
+    update(now, delta, note);
+  });
+}
+
+function update(now, delta, note) {
+  const diffTimeBetweenAnimationFrame = (now - delta) / MILISECOND;
+
+  note.positionY += diffTimeBetweenAnimationFrame * speed;
+
+  render(note);
+}
+
+function render(note) {
+  const sideBorderWidth = 2;
+
+  if (note.key === "s") {
+    note.positionX = columnWidth * 0;
+    note.color = "red";
+  }
+
+  if (note.key === "d") {
+    note.positionX = columnWidth * 1;
+    note.color = "blue";
+  }
+
+  if (note.key === "f") {
+    note.positionX = columnWidth * 2;
+    note.color = "yellow";
+  }
+
+  if (note.key === "j") {
+    note.positionX = columnWidth * 3;
+    note.color = "purple";
+  }
+
+  if (note.key === "k") {
+    note.positionX = columnWidth * 4;
+    note.color = "orange";
+  }
+
+  if (note.key === "l") {
+    note.positionX = columnWidth * 5;
+    note.color = "skyblue";
+  }
+
+  ctx.fillStyle = `${note.color}`;
+  ctx.fillRect(
+    note.positionX,
+    note.positionY,
+    columnWidth - sideBorderWidth * 2,
+    note.height || noteHeight
+  );
+
+  if (note.positionY >= height - 20) {
+    // console.log(time, "time"); // 캔버스 처음에서 내려오는 길이 계산용
+  }
+}
 
 fileInput.addEventListener("change", () => {
   const reader = new FileReader();
@@ -93,6 +247,7 @@ startButton.addEventListener("click", () => {
   }
 
   if (audioContext.state === "running") {
+    console.log(audioContext.state);
     return;
   }
 
@@ -104,77 +259,6 @@ startButton.addEventListener("click", () => {
 });
 
 stopButton.addEventListener("click", () => {
-  // cancelAnimationFrame(updateNotes);
   source.stop();
-});
-
-let delta = Date.now();
-
-function updateNotes() {
-  const now = Date.now();
-
   ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
-  time += (now - delta) / MILISECOND;
-
-  console.log(time);
-
-  const visibleNotes = notes.filter((note) => note.time <= time);
-  renderNotes(now, delta, visibleNotes);
-  requestAnimationFrame(updateNotes);
-
-  delta = now;
-}
-
-function renderNotes(now, delta, notes) {
-  notes.forEach((note) => {
-    if (note.time <= time) {
-      update(now, delta, note);
-    }
-  });
-}
-
-function update(now, delta, note) {
-  const diffTimeBetweenAnimationFrame = (now - delta) / MILISECOND;
-
-  note.positionY += diffTimeBetweenAnimationFrame * speed;
-
-  console.log("positionY:", note.positionY);
-
-  render(note);
-}
-
-function render(note) {
-  const sideBorderWidth = 2;
-
-  if (note.key === "a") {
-    positionX = columnWidth * 0;
-  }
-
-  if (note.key === "s") {
-    positionX = columnWidth * 1;
-  }
-
-  if (note.key === "d") {
-    positionX = columnWidth * 2;
-  }
-
-  if (note.key === "j") {
-    positionX = columnWidth * 3;
-  }
-
-  if (note.key === "k") {
-    positionX = columnWidth * 4;
-  }
-
-  if (note.key === "l") {
-    positionX = columnWidth * 5;
-  }
-
-  ctx.fillStyle = `${note.color}`;
-  ctx.fillRect(
-    positionX,
-    note.startPosition + note.positionY,
-    columnWidth - sideBorderWidth * 2,
-    noteHeight
-  );
-}
+});
